9098dc6a3de72451cdfeda420144b2c80e1bf200
whitzhu <whitzhu@gmail.com>
2017-02-23 11:31:18 -0800

    Complete searchYouTube Tests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,26 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+ 
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    contentType: 'application/json',
+    data: { 
+      part: 'snippet',
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      videoEmbeddable: 'true',
+      type: 'video'
+    },
+    success: function (data) {
+      console.log('Example video data:', exampleVideoData);
+      console.log('Success in getting data', data);
+      callback(data.items);
+    },
+    error: function (data) {
+      console.error ('Failed to get');
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
